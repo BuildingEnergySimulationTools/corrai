8e39ee9ba43f0091d502a81a080280c53565119b
Tesshub <thubert@nobatek.inef4.com>
2023-04-24 11:37:05 +0200

    adaptation of code for the new dictionary parameter declaration

--- corrai/multi_optimize.py
+++ corrai/multi_optimize.py
@@ -8,7 +8,7 @@ class ModelicaFunction:
     def __init__(self,
                  simulator,
                  param_dict,
-                 agg_method=np.mean,r
+                 agg_method=np.mean
                  ):
         self.simulator = simulator
         self.param_dict = param_dict
@@ -25,64 +25,24 @@ class ModelicaFunction:
         return self.agg_method(res, axis=0)
 
 
-class MyMixedProblem(ElementwiseProblem):
-    def __init__(self, parameters, mf_list, pyf_list, function_names, constraints_names):
-        global var
-        self.parameters = parameters
-        self.mf_list = mf_list
-        self.pyf_list = pyf_list
-        self.function_names = function_names
-        self.constraints_names = constraints_names
-        variable_string = {}
-
-        for k, v in parameters.items():
-            name, bounds, vtype = k, v[:2], v[2]
-            if vtype == "Integer":
-                var = Integer(bounds=bounds)
-            elif vtype == "Real":
-                var = Real(bounds=bounds)
-            # elif vtype == "Choice":
-            #     var = Choice(options=bounds)
-            # variable_string[name] = var
-
-        super().__init__(vars=variable_string,
-                         n_var=len(parameters),
-                         n_obj=len(function_names),
-                         n_ieq_constr=len(constraints_names),
-                         xl=np.array([val[0] for val in parameters.values()]),
-                         xu=np.array([val[1] for val in parameters.values()]),
-                         )
-
-    def _evaluate(self, X, out, *args, **kwargs):
-        #current_param = {key: val for key, val in zip(self.parameters.keys(), x)}
-        res = pd.concat(
-            [m.function(X) for m in self.mf_list] +
-            [pyf(X) for pyf in self.pyf_list]
-        )
-
-        out["F"] = list(res[self.function_names])
-        out["G"] = list(res[self.constraints_names])
-
 class MyProblem(ElementwiseProblem):
-    def __init__(self, parameters, mf_list, pyf_list, function_names, constraints_names):
+    def __init__(self, parameters, mf_list, pyf_list, function_names, constraint_names):
         self.parameters = parameters
         self.mf_list = mf_list
         self.pyf_list = pyf_list
         self.function_names = function_names
-        self.constraints_names = constraints_names
+        self.constraints_names = constraint_names
         variable_string = {}
-
-
 
         super().__init__(n_var=len(parameters),
                          n_obj=len(function_names),
-                         n_ieq_constr=len(constraints_names),
-                         xl=np.array([val[0] for val in parameters.values()]),
-                         xu=np.array([val[1] for val in parameters.values()]),
+                         n_ieq_constr=len(constraint_names),
+                         xl=np.array([p['bounds'][0] for p in parameters]),
+                         xu=np.array([p['bounds'][-1] for p in parameters]),
                          )
 
     def _evaluate(self, x, out, *args, **kwargs):
-        current_param = {key: val for key, val in zip(self.parameters.keys(), x)}
+        current_param = {param['name']: val for param, val in zip(self.parameters, x)}
         res = pd.concat(
             [m.function(current_param) for m in self.mf_list] +
             [pyf(current_param) for pyf in self.pyf_list]
@@ -91,3 +51,40 @@ class MyProblem(ElementwiseProblem):
         out["F"] = list(res[self.function_names])
         out["G"] = list(res[self.constraints_names])
 
+
+class MyMixedProblem(ElementwiseProblem):
+    def __init__(self, parameters, mf_list, pyf_list, function_names, constraint_names):
+        global var
+        self.parameters = parameters
+        self.mf_list = mf_list
+        self.pyf_list = pyf_list
+        self.function_names = function_names
+        self.constraints_names = constraint_names
+        variable_string = {}
+
+        for param in parameters:
+            name, bounds, vtype = param['name'], param['bounds'], param['type']
+            if vtype == "Integer":
+                var = Integer(bounds=bounds)
+            elif vtype == "Real":
+                var = Real(bounds=bounds)
+            elif vtype == "Choice":
+                var = Choice(options=bounds)
+            variable_string[name] = var
+
+        super().__init__(vars=variable_string,
+                         n_var=len(parameters),
+                         n_obj=len(function_names),
+                         n_ieq_constr=len(constraint_names),
+                         xl=np.array([p['bounds'][0] for p in parameters]),
+                         xu=np.array([p['bounds'][-1] for p in parameters]),
+                         )
+
+    def _evaluate(self, X, out, *args, **kwargs):
+        res = pd.concat(
+            [m.function(X) for m in self.mf_list] +
+            [pyf(X) for pyf in self.pyf_list]
+        )
+
+        out["F"] = list(res[self.function_names])
+        out["G"] = list(res[self.constraints_names])
