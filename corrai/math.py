1be1cf80e26c4bb1e6cc7232b66b348b060078cc
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-09-15 12:15:27 +0200

    ♻️ mixin deleted, class transformed to function and moved to math module

--- corrai/math.py
+++ corrai/math.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 from scipy import integrate
+from collections.abc import Callable
 
 from corrai.utils import as_1_column_dataframe
 from corrai.utils import check_datetime_index
@@ -137,3 +138,69 @@ def time_integrate(
         res_series[col] = integrate.trapz(selected_ts[col], chrono)
 
     return res_series
+
+
+def aggregate_time_series(
+    result_df: pd.DataFrame,
+    agg_method: Callable = np.mean,
+    agg_method_kwarg: dict = {},
+    reference_df: pd.DataFrame | None = None,
+) -> pd.Series:
+    """
+    A function to perform data aggregation operations on a given DataFrame using a
+    specified aggregation method. It also supports aggregation with respect to a
+    reference DataFrame (eg. for error functions).
+
+    Parameters:
+    - result_df (pd.DataFrame): The DataFrame containing the data to be aggregated.
+    - agg_method (Callable, optional): The aggregation method to be applied. Default is
+        np.sum.
+    - agg_method_kwarg (dict, optional): Additional keyword arguments to be passed to
+        the aggregation method. Default is an empty dictionary.
+    - reference_df (pd.DataFrame | None, optional): A reference DataFrame for error
+        function aggregation. If provided, both result_df and reference_df should have
+        the same shape. Default is None.
+
+    Returns:
+    - pd.Series: A pandas Series containing the aggregated values with column names as
+        indices.
+
+    Raises:
+    - ValueError: If reference_df is provided and result_df and reference_df have
+      inconsistent shapes.
+
+    Example usage:
+        >>> mixin = AggregationMixin()
+        >>> result_df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+        >>> agg_series = mixin.get_aggregated(result_df)
+        >>> print(agg_series)
+    A    2
+    B    5
+    dtype: int64
+    """
+
+    check_datetime_index(result_df)
+
+    if reference_df is not None:
+        check_datetime_index(reference_df)
+        if not result_df.shape == reference_df.shape:
+            raise ValueError(
+                "Cannot perform aggregation results_df and "
+                "reference_df have inconsistent shapes"
+            )
+        return pd.Series(
+            [
+                agg_method(result_df[col], reference_df.iloc[:, i], **agg_method_kwarg)
+                for i, col in enumerate(result_df.columns)
+            ],
+            index=result_df.columns,
+        )
+
+    else:
+        return pd.Series(
+            [
+                agg_method(result_df[col], **agg_method_kwarg)
+                for col in result_df.columns
+            ],
+            index=result_df.columns,
+        )
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from corrai.math import time_gradient
-from corrai.math import time_integrate
+from corrai.math import time_gradient, time_integrate, aggregate_time_series
+from corrai.metrics import nmbe
 
 
 class TestMath:
@@ -65,3 +65,25 @@ class TestMath:
         ref = pd.Series({"cpt1": 3.0, "cpt2": 3.0})
 
         pd.testing.assert_series_equal(ref, time_integrate(test) / 3600)
+
+    def test_aggregate_time_series(self):
+        sim_res = pd.DataFrame(
+            {"a": [1, 2], "b": [3, 4]},
+            index=pd.date_range("2009-01-01", freq="H", periods=2),
+        )
+        ref_df = pd.DataFrame(
+            {"a": [1, 1], "b": [3, 4]},
+            index=pd.date_range("2009-01-01", freq="H", periods=2),
+        )
+
+        expected_default = pd.Series([1.5, 3.5], index=["a", "b"])
+
+        expected_nmbe = pd.Series([50.0, 0.0], index=["a", "b"])
+
+        pd.testing.assert_series_equal(
+            aggregate_time_series(sim_res), expected_default
+        )
+        pd.testing.assert_series_equal(
+            aggregate_time_series(sim_res, agg_method=nmbe, reference_df=ref_df),
+            expected_nmbe,
+        )
