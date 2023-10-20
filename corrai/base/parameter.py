2c4a545ddcbb0e774b320ff7772d664e6bc224ba
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-10-20 11:30:24 +0200

    📝 Doc update

--- corrai/base/parameter.py
+++ corrai/base/parameter.py
@@ -6,8 +6,9 @@ class Parameter(Enum):
     NAME: the model path to the parameter
     INTERVAL: a tuple indicating parameter bounds
     INIT_VALUE: the parameter initial value
-    TYPE: Integer, Real
+    TYPE: Integer, Real, Choice, Binary
     """
+
     NAME = "NAME"
     INTERVAL = "INTERVAL"
     INIT_VALUE = "INIT_VALUE"
