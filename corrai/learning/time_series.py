811b8bc79c4a952350477a79b85e129758dc1062
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-11-24 18:07:00 +0100

    ✨ plot_sequence_forcast

--- corrai/learning/time_series.py
+++ corrai/learning/time_series.py
@@ -1,7 +1,62 @@
 from abc import ABC, abstractmethod
 
 import keras
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
+
+
+def plot_sequence_forcast(X, y_ref, model, X_target_index=0, batch_nb=0):
+    """
+    Plot the historical, reference, and predicted values for a given
+    batch number in X.
+
+    Parameters:
+    :param X: np.ndarray 3D array of shape [batch_size, time_step, dimension]
+    Input sequences.
+    :param y_ref: np.ndarray of shape [batch_size, time_step].
+    :param model: keras.Model The trained model for making predictions.
+    :param X_target_index: The index of 3rd dimension in X that contains
+        historical values.
+    :param batch_nb: int Batch number to visualize. Default is 0.
+    """
+    predictions = model.predict(X)
+    predictions = predictions[batch_nb, :]
+    y_ref_to_plot = y_ref[batch_nb, :]
+    x_to_plot = X[batch_nb, :, X_target_index]
+
+    fig, ax = plt.subplots()
+    # Plot historic values
+    ax.plot(
+        range(len(x_to_plot)),
+        x_to_plot,
+        marker="o",
+        label="Historic Values",
+    )
+
+    # Plot new values
+    ax.plot(
+        range(len(x_to_plot), len(x_to_plot) + len(y_ref_to_plot)),
+        y_ref_to_plot,
+        marker="o",
+        label="New Values",
+    )
+
+    # Plot predicted values
+    ax.plot(
+        range(len(x_to_plot), len(x_to_plot) + len(y_ref_to_plot)),
+        predictions,
+        marker="o",
+        label="Predicted Values",
+    )
+
+    # Set labels and title
+    ax.set_xlabel("Number of Timesteps")
+    ax.set_ylabel("x(t)")
+    ax.set_title("Actual and Predicted Values Over Time")
+    ax.legend()
+
+    plt.show()
 
 
 class KerasModelSkBC(ABC, BaseEstimator, RegressorMixin):
