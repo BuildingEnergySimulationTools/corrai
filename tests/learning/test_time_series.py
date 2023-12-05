fa72a28b33eb2b5416d65c274e7029541316de39
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2023-12-05 10:24:23 +0100

    ✅ ts linear replaced by ts deep nn

--- tests/learning/test_time_series.py
+++ tests/learning/test_time_series.py
@@ -5,13 +5,13 @@ from corrai.learning.model_selection import (
     sequences_train_test_split,
 )
 from corrai.learning.time_series import (
-    TsLinearModel,
+    TsDeepNN,
     reshape_target_sequence_to_sequence,
 )
 
 
 class TestTimeSeries:
-    def test_ts_linear_model(self):
+    def test_ts_deep_nn(self):
         datas = pd.DataFrame({"data": np.arange(10000)})
 
         sequences = time_series_sampling(datas, sequence_length=6)
@@ -19,7 +19,7 @@ class TestTimeSeries:
             sequences, n_steps_history=4, n_steps_future=2
         )
 
-        ts_linear = TsLinearModel()
+        ts_linear = TsDeepNN()
         ts_linear.fit(x_train, y_train)
 
         assert ts_linear.score(x_test, y_test) > 0.999
