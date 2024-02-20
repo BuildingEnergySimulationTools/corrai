079c59d618890f1a818383b59d7c6ef681ebf658
Tesshub <thubert@nobatek.inef4.com>
2024-02-20 10:58:21 +0100

    🐛Fix of math.py for aggregation with reference

--- corrai/math.py
+++ corrai/math.py
@@ -157,9 +157,10 @@ def aggregate_time_series(
         np.sum.
     - agg_method_kwarg (dict, optional): Additional keyword arguments to be passed to
         the aggregation method. Default is an empty dictionary.
-    - reference_df (pd.DataFrame | None, optional): A reference DataFrame for error
-        function aggregation. If provided, both result_df and reference_df should have
-        the same shape. Default is None.
+    - reference_df (pd.DataFrame | pd.Series None, optional):
+        reference one column DataFrame for error function aggregation.
+        If provided, both result_df and reference_df should have
+        the same first dimension. Default is None.
 
     Returns:
     - pd.Series: A pandas Series containing the aggregated values with column names as
@@ -185,7 +186,7 @@ def aggregate_time_series(
 
     if reference_df is not None:
         check_datetime_index(reference_df)
-        if not result_df.shape == reference_df.shape:
+        if not result_df.shape[0] == reference_df.shape[0]:
             raise ValueError(
                 "Cannot perform aggregation results_df and "
                 "reference_df have inconsistent shapes"
@@ -193,7 +194,7 @@ def aggregate_time_series(
         return pd.Series(
             [
                 agg_method(
-                    result_df.iloc[:, i], reference_df.iloc[:, i], **agg_method_kwarg
+                    result_df.iloc[:, i], reference_df.iloc[:, 0], **agg_method_kwarg
                 )
                 for i in range(len(result_df.columns))
             ],
