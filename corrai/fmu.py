3193dcf925f2485d40bd1b18c4fcc986aab4a93a
Tesshub <thubert@nobatek.inef4.com>
2024-03-13 12:09:16 +0100

    📝Docstring

--- corrai/fmu.py
+++ corrai/fmu.py
@@ -10,7 +10,50 @@ from modelitool.combitabconvert import seconds_to_datetime, df_to_combitimetable
 from corrai.base.model import Model
 
 
-class FMUModel(Model):
+class FmuModel(Model):
+    """
+    A model class for simulating FMUs (Functional Mock-up Units)
+        in a standardized and flexible way,integrating various data
+        sources and simulation parameters.
+
+    The `FmuModel` class provides functionalities for setting up and
+        running simulations of FMUs, including handling initial parameters,
+        boundary conditions, and simulation options.It is designed to
+        facilitate easy integration of simulation results into larger workflows,
+        with support for converting boundary conditions from Pandas DataFrames
+        and adjusting simulation parameters dynamically.
+
+
+    Methods:
+
+        set_simulation_options(self, simulation_options):
+            Updates the simulation options.
+
+        set_boundaries_df(self, df):
+            Sets boundary conditions from a Pandas DataFrame and updates
+                the model's initial parameters to include these boundaries.
+
+        set_param_dict(self, param_dict):
+            Updates the initial parameters of the model with
+                the values from `param_dict`.
+
+        simulate: Runs the FMU simulation with the specified parameters and options,
+             returning the results as a Pandas DataFrame indexed by datetime.
+
+            Parameters:
+                parameter_dict (dict, optional): Parameter values to
+                    override or update before simulation. simulation_options:
+                    Simulation options to override or update before simulation.
+                debug_logging (bool, optional): Enables or disables debug
+                    logging for the simulation.
+                logger (logging.Logger, optional): A logging.Logger instance
+                 for recording simulation logs.
+
+            Returns:
+                pd.DataFrame: A DataFrame containing the simulation results,
+                    with timestamps as the index.
+    """
+
     def __init__(
         self,
         model_path,
@@ -20,6 +63,37 @@ class FMUModel(Model):
         boundary_df=None,
         year=None,
     ):
+        """
+        Initialize an instance of the FmuModel class.
+
+        This method sets up the model with necessary paths, simulation options,
+            output variables, initial parameters, boundary conditions,
+            and the simulation year.
+
+        Parameters:
+            model_path (str or Path): The file path to the FMU model.
+                Can be a string or a Path object.
+            simulation_options (dict): Options for configuring the simulation,
+                such as start time, stop time, step size, solver, etc.
+            output_list (list of str): A list of output variables
+                that the simulation should record.
+            init_parameters (dict, optional): Initial values for
+                parameters within the FMU model. Defaults to an
+                empty dict if None is provided.
+            boundary_df (pd.DataFrame, optional): A DataFrame containing
+                boundary conditions for the simulation. Must have datetime indices.
+                If provided, it will set initial conditions based on this data.
+            year (int, optional): The year in which the simulation's
+                time series starts. This is important for aligning
+                the simulation results with real-world dates.
+                Ignored if `boundary_df` is provided since the year
+                will be derived from the DataFrame's index.
+
+        Raises:
+            Warning: If both `boundary_df` and `year` are provided,
+                a warning is raised indicating that `year` will be
+                ignored and derived from `boundary_df` instead.
+        """
         self.model_path = (
             Path(model_path) if isinstance(model_path, str) else model_path
         )
@@ -44,11 +118,41 @@ class FMUModel(Model):
             self.year = dt.date.today().year
 
     def set_simulation_options(self, simulation_options):
+        """
+        Update the simulation options for the model.
+
+        This method allows for dynamic adjustment of the
+            simulation settings, such as changing the solver
+            or the time frame of the simulation.
+
+        Parameters:
+            simulation_options (dict): A dictionary containing the
+                simulation options to be updated. Options might
+                include 'startTime', 'stopTime', 'stepSize', 'solver',
+                'outputInterval', and 'fmi_type'.
+        """
         self.simulation_options = simulation_options
 
     def set_boundaries_df(self, df):
+        """
+        Set boundary conditions for the simulation
+            from a Pandas DataFrame.
+
+        The DataFrame should contain time series data
+            that will be used to generate a boundary
+            condition file.The index of the DataFrame
+            must be a DateTimeIndex, as it determines
+            the simulation's time frame.
+
+        Parameters:
+            df (pd.DataFrame): A DataFrame containing the boundary
+                conditions with a DateTimeIndex.
+
+        Raises:
+            ValueError: If the DataFrame's index is not a DateTimeIndex,
+            an error is raised indicating the requirement for datetime indices.
+        """
         new_bounds_path = self._simulation_path / "boundaries.txt"
-        print(new_bounds_path)
         df_to_combitimetable(df, new_bounds_path)
 
         if self.init_parameters is None:
@@ -66,6 +170,17 @@ class FMUModel(Model):
             )
 
     def set_param_dict(self, param_dict):
+        """
+        Update the initial parameter values for the model.
+
+        This method allows for the dynamic setting or updating of
+            initial parameters before running the simulation.
+
+        Parameters:
+            param_dict (dict): A dictionary where keys are parameter
+                names and values are the corresponding values
+                to be set or updated in the model.
+        """
         if self.init_parameters is None:
             self.init_parameters = {}
         self.init_parameters.update(param_dict)
@@ -77,6 +192,29 @@ class FMUModel(Model):
         debug_logging=False,
         logger=None,
     ) -> pd.DataFrame:
+        """
+        Run the simulation with the current model configuration.
+
+        This method executes the FMU simulation according to the specified
+            parameters and options, and returns the simulation
+            results as a Pandas DataFrame indexed by datetime.
+
+        Parameters:
+            parameter_dict (dict, optional): Additional or overriding
+             parameter values for this simulation run.
+            simulation_options (dict, optional): Additional or
+                overriding   simulation options for this run.
+            debug_logging (bool, optional): Whether to enable
+                detailed logging of the simulation process. Defaults to False.
+            logger (logging.Logger, optional): A logger for capturing
+                simulation logs, if debug_logging is True.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the simulation results.
+                Columns include 'time' and the specified output variables,
+                with the DataFrame indexed by datetime reflecting
+                the simulation period.
+        """
         if parameter_dict:
             self.set_param_dict(parameter_dict)
         if simulation_options:
