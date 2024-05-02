c48c14a6c62c43e7d0304ad8eb80ae8fcd5bd723
Tesshub <thubert@nobatek.inef4.com>
2024-05-02 17:43:55 +0200

    👽️ SimulationSampler (with simulator and parameters) added

--- corrai/learning/sampling.py
+++ corrai/learning/sampling.py
@@ -1,34 +1,61 @@
 import numpy as np
+from corrai.base.parameter import Parameter
+
+from scipy.stats.qmc import LatinHypercube
+from fastprogress.fastprogress import force_console_behavior
+
+master_bar, progress_bar = force_console_behavior()
 
 
-class Sampler:
-    def __init__(self):
-        self.samples = None
+class SimulationSampler:
+    def __init__(self, parameters, simulator, sampling_method="LatinHypercube"):
+        self.parameters = parameters
+        self.simulator = simulator
+        self.sampling_method = sampling_method
+        self.sample = np.empty(shape=(0, len(parameters)))
+        self.sample_results = []
+        if sampling_method == "LatinHypercube":
+            self.sampling_method = LatinHypercube
 
-    def add_samples(self, new_samples):
-        """
-        Add new samples to the existing sample set.
+    def get_boundary_sample(self):
+        return np.array(
+            [[par[Parameter.INTERVAL][i] for i in range(2)] for par in self.parameters]
+        )
 
-        Parameters:
-        - new_samples (numpy.ndarray or list): New samples to add. Each row
-            represents a sample.
-        """
-        if self.samples is None:
-            self.samples = new_samples
-        else:
-            self.samples = np.vstack([self.samples, new_samples])
+    def add_sample(self, sample_size, seed=None):
+        sampler = LatinHypercube(d=len(self.parameters), seed=seed)
+        new_sample = sampler.random(n=sample_size)
+        new_sample_value = np.empty(shape=(0, len(self.parameters)))
+        for s in new_sample:
+            new_sample_value = np.vstack(
+                (
+                    new_sample_value,
+                    [
+                        par[Parameter.INTERVAL][0]
+                        + val
+                        * (par[Parameter.INTERVAL][1] - par[Parameter.INTERVAL][0])
+                        for par, val in zip(self.parameters, s)
+                    ],
+                )
+            )
+        if self.sample.size == 0:
+            bound_sample = self.get_boundary_sample()
+            bound_sample = np.hstack(
+                (
+                    bound_sample,
+                    np.tile(bound_sample[:, [-1]], (1, new_sample_value.shape[1] - 2)),
+                )
+            )
+            new_sample_value = np.vstack((new_sample_value, bound_sample))
 
-    def get_samples(self):
-        """
-        Get the current sample set.
+        prog_bar = progress_bar(range(new_sample_value.shape[0]))
+        for _, simul in zip(prog_bar, new_sample_value):
+            sim_config = {
+                par[Parameter.NAME]: val for par, val in zip(self.parameters, simul)
+            }
+            prog_bar.comment = "Simulations"
 
-        Returns:
-        - samples (numpy.ndarray or None): The current sample set.
-        """
-        return self.samples
+            results = self.simulator.simulate(parameter_dict=sim_config)
+            self.sample_results.append(results)
 
-    def clear_samples(self):
-        """
-        Clear the current sample set.
-        """
-        self.samples = None
+        self.sample = np.vstack((self.sample, new_sample_value))
