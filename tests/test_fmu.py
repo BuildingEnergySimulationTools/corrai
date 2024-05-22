2f031085ecfc8db3b7d102b3b71c9e5d31c66e98
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2024-05-22 18:15:20 +0200

    ♻️ ModelicaFmuModel refactoring
    🐛 date time Start Stop fix
    
    🚧 FMU refactoring work in progress

--- tests/test_fmu.py
+++ tests/test_fmu.py
@@ -1,14 +1,14 @@
-import pandas as pd
-import numpy as np
+import datetime as dt
+import os
+import platform
+import tempfile
 from pathlib import Path
+
+import numpy as np
+import pandas as pd
 import pytest
 
-import tempfile
-import os
-
-import platform
-
-from corrai.fmu import FmuModel
+from corrai.fmu import ModelicaFmuModel
 
 system = platform.system()
 
@@ -25,6 +25,7 @@ def simul():
     simul_options = {
         "startTime": 0,
         "stopTime": 2,
+        "stepSize": 1,
         "solver": "CVode",
         "outputInterval": 1,
         "tolerance": 1e-6,
@@ -32,8 +33,8 @@ def simul():
 
     outputs = ["res.showNumber"]
 
-    simu = FmuModel(
-        model_path=PACKAGE_DIR / "rosen.fmu",
+    simu = ModelicaFmuModel(
+        fmu_path=PACKAGE_DIR / "rosen.fmu",
         simulation_options=simul_options,
         output_list=outputs,
     )
@@ -55,8 +56,8 @@ outputs = ["Boundaries.y[1]", "Boundaries.y[2]"]
 
 @pytest.fixture()
 def simul_boundaries_int():
-    simu = FmuModel(
-        model_path=PACKAGE_DIR / "TestLib.boundary_test.fmu",
+    simu = ModelicaFmuModel(
+        fmu_path=PACKAGE_DIR / "TestLib.boundary_test.fmu",
         simulation_options=simul_options,
         output_list=outputs,
     )
@@ -65,8 +66,8 @@ def simul_boundaries_int():
 
 @pytest.fixture()
 def simul_boundaries():
-    simu = FmuModel(
-        model_path=PACKAGE_DIR / "boundary_test.fmu",
+    simu = ModelicaFmuModel(
+        fmu_path=PACKAGE_DIR / "boundary_test.fmu",
         simulation_options=simul_options,
         output_list=outputs,
     )
@@ -74,15 +75,6 @@ def simul_boundaries():
 
 
 class TestFmu:
-    def test_set_param_dict(self, simul):
-        test_dict = {
-            "x.k": 2.0,
-            "y.k": 3.0,
-        }
-        simul.set_param_dict(test_dict)
-        modified_params = simul.check_parameter_modifications()
-        assert test_dict == modified_params
-
     def test_results(self, simul):
         res = simul.simulate()
         ref = pd.DataFrame({"res.showNumber": [401.0, 401.0, 401.0]})
@@ -90,7 +82,7 @@ class TestFmu:
 
     if system == "Windows":
         # Because issues with relatives filepaths exporting FMUs from OM.
-        def test_set_boundaries_df(self, simul_boundaries, simul_boundaries_int):
+        def test_simulate(self, simul_boundaries, simul_boundaries_int):
             new_bounds = pd.DataFrame(
                 {"Boundaries.y[1]": [1, 2, 3], "Boundaries.y[2]": [3, 4, 5]},
                 index=pd.date_range("2009-07-13 00:00:00", periods=3, freq="H"),
@@ -99,21 +91,34 @@ class TestFmu:
             new_bounds = new_bounds.astype(float)
 
             res1 = simul_boundaries_int.simulate()
-
-            simul_boundaries.set_boundaries_df(new_bounds)
-            res2 = simul_boundaries.simulate()
+            res2 = simul_boundaries.simulate(x=new_bounds)
 
             assert np.allclose(res1.values, res2.values)
 
-    def test_set_boundaries_value_error(self, simul_boundaries):
-        invalid_df = pd.DataFrame(
-            {"Boundaries.y[1]": [1, 2, 3], "Boundaries.y[2]": [3, 4, 5]},
-            index=pd.Index([1, 2, 3]),
-        )
-        invalid_df = invalid_df.astype(float)
+            x_datetime = pd.DataFrame(
+                {
+                    "Boundaries.y[1]": [1, 2, 3, 4, 5],
+                    "Boundaries.y[2]": [3, 4, 5, 6, 7],
+                },
+                index=pd.date_range("2009-07-13 00:00:00", periods=5, freq="H"),
+            )
 
-        with pytest.raises(ValueError):
-            simul_boundaries.set_boundaries_df(invalid_df)
+            simul_boundaries.simulate(
+                x=x_datetime,
+                simulation_options={
+                    "startTime": dt.datetime(
+                        year=2009, month=7, day=13, hour=1, minute=0, second=0
+                    ),
+                    "stopTime": dt.datetime(
+                        year=2009, month=7, day=13, hour=2, minute=0, second=0
+                    ),
+                },
+            )
+
+            with pytest.raises(TypeError):
+                simul_boundaries.simulate(
+                    x=x_datetime, simulation_options={"startTime": 1, "stopTime": 2.0}
+                )
 
     def test_save(self, simul):
         with tempfile.TemporaryDirectory() as temp_dir:
