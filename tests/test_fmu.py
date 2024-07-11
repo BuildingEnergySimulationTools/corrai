96e9b9d5c5c986c4fb03bd23f34833c8de5c2123
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2024-07-11 17:00:11 +0200

    ⚡️ fmu rework

--- tests/test_fmu.py
+++ tests/test_fmu.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
-import pytest
 
 from corrai.fmu import ModelicaFmuModel
 
@@ -20,80 +19,65 @@ else:
     raise NotImplementedError(f"Unsupported operating system: {system}")
 
 
-@pytest.fixture()
-def simul():
-    simul_options = {
-        "startTime": 0,
-        "stopTime": 2,
-        "stepSize": 1,
-        "solver": "CVode",
-        "outputInterval": 1,
-        "tolerance": 1e-6,
-    }
-
-    outputs = ["res.showNumber"]
-
-    simu = ModelicaFmuModel(
-        fmu_path=PACKAGE_DIR / "rosen.fmu",
-        simulation_options=simul_options,
-        output_list=outputs,
-    )
-    return simu
-
-
-simul_options = {
-    "startTime": 16675200,
-    "stopTime": 16682400,
-    "outputInterval": 3600,
-    "solver": "Euler",
-    "stepSize": 3600,
-    "tolerance": 1e-6,
-    "fmi_type": "ModelExchange",
-}
-
-outputs = ["Boundaries.y[1]", "Boundaries.y[2]"]
-
-
-@pytest.fixture()
-def simul_boundaries_int():
-    simu = ModelicaFmuModel(
-        fmu_path=PACKAGE_DIR / "TestLib.boundary_test.fmu",
-        simulation_options=simul_options,
-        output_list=outputs,
-    )
-    return simu
-
-
-@pytest.fixture()
-def simul_boundaries():
-    simu = ModelicaFmuModel(
-        fmu_path=PACKAGE_DIR / "boundary_test.fmu",
-        simulation_options=simul_options,
-        output_list=outputs,
-    )
-    return simu
-
-
 class TestFmu:
-    def test_results(self, simul):
-        res = simul.simulate()
+    def test_results(self):
+        simul_options = {
+            "startTime": 0,
+            "stopTime": 2,
+            "stepSize": 1,
+            "solver": "CVode",
+            "outputInterval": 1,
+            "tolerance": 1e-6,
+        }
+
+        simu = ModelicaFmuModel(
+            fmu_path=PACKAGE_DIR / "rosen.fmu",
+            simulation_options=simul_options,
+            output_list=["res.showNumber"],
+        )
+
+        res = simu.simulate()
         ref = pd.DataFrame({"res.showNumber": [401.0, 401.0, 401.0]})
         assert np.allclose(res["res.showNumber"].values, ref["res.showNumber"].values)
 
     if system == "Windows":
         # Because issues with relatives filepaths exporting FMUs from OM.
-        def test_simulate(self, simul_boundaries, simul_boundaries_int):
+        def test_simulate(self):
+            simu = ModelicaFmuModel(
+                fmu_path=PACKAGE_DIR / "boundary_test.fmu",
+                output_list=["Boundaries.y[1]", "Boundaries.y[2]"],
+            )
+
             new_bounds = pd.DataFrame(
                 {"Boundaries.y[1]": [1, 2, 3], "Boundaries.y[2]": [3, 4, 5]},
-                index=pd.date_range("2009-07-13 00:00:00", periods=3, freq="H"),
+                index=range(3, 6),
             )
+
             new_bounds.index.freq = None
             new_bounds = new_bounds.astype(float)
 
-            res1 = simul_boundaries_int.simulate()
-            res2 = simul_boundaries.simulate(x=new_bounds)
+            res = simu.simulate(
+                x=new_bounds,
+                simulation_options={
+                    "solver": "CVode",
+                    "outputInterval": 1,
+                    "stepSize": 1,
+                },
+                solver_duplicated_keep="last",
+            )
 
-            assert np.allclose(res1.values, res2.values)
+            assert res.to_dict() == {
+                "Boundaries.y[1]": {
+                    3.0: 1.0000000000000333,
+                    4.0: 2.0000000000000453,
+                    5.0: 3.0,
+                },
+                "Boundaries.y[2]": {
+                    3.0: 3.0000000000000333,
+                    4.0: 4.000000000000045,
+                    5.0: 5.0,
+                },
+            }
 
             x_datetime = pd.DataFrame(
                 {
@@ -102,30 +86,41 @@ class TestFmu:
                 },
                 index=pd.date_range("2009-07-13 00:00:00", periods=5, freq="H"),
             )
+            x_datetime.index.name = "time"
+            x_datetime.index.freq = None
+            x_datetime = x_datetime.astype(float)
 
-            simul_boundaries.simulate(
+            res = simu.simulate(
                 x=x_datetime,
                 simulation_options={
-                    "startTime": dt.datetime(
-                        year=2009, month=7, day=13, hour=1, minute=0, second=0
-                    ),
-                    "stopTime": dt.datetime(
-                        year=2009, month=7, day=13, hour=2, minute=0, second=0
-                    ),
+                    "outputInterval": 3600,
+                    "stepSize": 3600,
                 },
+                solver_duplicated_keep="last",
             )
 
-            with pytest.raises(TypeError):
-                simul_boundaries.simulate(
-                    x=x_datetime, simulation_options={"startTime": 1, "stopTime": 2.0}
-                )
+            pd.testing.assert_frame_equal(res, x_datetime)
 
-    def test_save(self, simul):
+            res = simu.simulate(
+                simulation_options={
+                    "startTime": dt.datetime(2009, 7, 13, 0, 0, 0),
+                    "stopTime": dt.datetime(2009, 7, 13, 3, 0, 0),
+                    "outputInterval": 3600,
+                },
+                solver_duplicated_keep="last",
+            )
+
+            pd.testing.assert_frame_equal(res, x_datetime.loc[:"2009-7-13 03:00:00", :])
+
+    def test_save(self):
+        simu = ModelicaFmuModel(
+            fmu_path=PACKAGE_DIR / "rosen.fmu",
+            output_list=["res.showNumber"],
+        )
+
         with tempfile.TemporaryDirectory() as temp_dir:
             file_path = os.path.join(temp_dir, "test_model.fmu")
 
-            simul.save(Path(file_path))
+            simu.save(Path(file_path))
             assert os.path.exists(temp_dir)
             assert "test_model.fmu" in os.listdir(temp_dir)
-
-            # os.remove(temp_dir)
