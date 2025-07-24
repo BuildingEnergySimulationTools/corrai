61d7092529f493e684a802b931b59b73bff0ac05
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2025-07-24 16:17:32 +0200

    ⚡️ invert argument position to comply with scikit-learn format

--- corrai/metrics.py
+++ corrai/metrics.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.utils import check_consistent_length
 
-def nmbe(y_pred, y_true):
+
+def nmbe(y_true, y_pred):
     """Normalized Mean Biased Error
 
     :param y_pred: array-like of shape (n_samples,) or (n_samples, n_outputs)
@@ -15,14 +16,11 @@ def nmbe(y_pred, y_true):
     check_consistent_length(y_pred, y_true)
     y_pred = y_pred.to_numpy() if isinstance(y_pred, pd.DataFrame) else y_pred
     y_true = y_true.to_numpy() if isinstance(y_true, pd.DataFrame) else y_true
-    # y_type, y_true, y_pred, multioutput = _check_reg_targets(
-    #     y_true, y_pred, "uniform_average"
-    # )
 
     return np.sum(y_pred - y_true) / np.sum(y_true) * 100
 
 
-def cv_rmse(y_pred, y_true):
+def cv_rmse(y_true, y_pred):
     """Coefficient of variation of root mean squared error
 
     :param y_pred: array-like of shape (n_samples,) or (n_samples, n_outputs)
@@ -35,9 +33,7 @@ def cv_rmse(y_pred, y_true):
     check_consistent_length(y_pred, y_true)
     y_pred = y_pred.to_numpy() if isinstance(y_pred, pd.DataFrame) else y_pred
     y_true = y_true.to_numpy() if isinstance(y_true, pd.DataFrame) else y_true
-    # y_type, y_true, y_pred, multioutput = _check_reg_targets(
-    #     y_true, y_pred, "uniform_average"
-    # )
+
     return (
         (1 / np.mean(y_true))
         * np.sqrt(np.sum((y_true - y_pred) ** 2) / (y_true.shape[0] - 1))
