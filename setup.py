de08a2aa58624c4ed7258686369cd966d9941768
BaptisteDE <bdurandestebe@nobatek.inef4.com>
2025-08-27 11:02:18 +0200

    🐛  licence

--- setup.py
+++ setup.py
@@ -16,10 +16,8 @@ setup(
     url="https://github.com/BuildingEnergySimulationTools/corrai",
     author="Nobatek/INEF4",
     author_email="bdurandestebe@nobatek.inef4.com",
-    license="License :: OSI Approved :: BSD License",
-    # keywords=[
-    # ],
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    license="BSD-3-Clause",
+    license_files=["LICENSE.md"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
